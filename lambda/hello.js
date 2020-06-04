a991d37b9533b817cb2044f36b0d8371d51d104e
Rohan <rohan.jaiswal@moonraft.com>
2020-06-04 20:00:08 +0530

    test lambda functions

--- lambda/hello.js
+++ lambda/hello.js
@@ -1,7 +1,7 @@
 
 // For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
 var fs = require('fs');
-var x = fs.readFileSync('../src/pages/news/2019-10-01-test.md');
+var x = fs.readFileSync('../public/page-data/news/2019-10-01-test/page-data.json');
 module.exports.handler = async function(event, context) {
   console.log("queryStringParameters", event.queryStringParameters)
   return {
@@ -10,6 +10,7 @@ module.exports.handler = async function(event, context) {
     body: JSON.stringify(x.toString())
   }
 }
+console.log(x.toString());
 // Now you are ready to access this API from anywhere in your Gatsby app! For example, in any event handler or lifecycle method, insert:
 // fetch("/.netlify/functions/hello")
 //    .then(response => response.json())
